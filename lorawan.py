2bded8844cf752b2964623187ad1c2ad46b586af
Oliv4945 <oliv4945@gmail.com>
2021-09-08 21:57:38 +0200

    Add event callback and main loop

--- lorawan.py
+++ lorawan.py
@@ -1,5 +1,34 @@
 from rak3172 import RAK3172
+import signal
 import sys
+
+
+class STATES:
+    JOINING = 0
+    JOINED = 1
+    SEND_DATA = 2
+    SLEEP = 3
+
+
+state = None
+
+
+def events(type, parameters):
+    global state
+
+    if type == RAK3172.EVENTS.JOINED:
+        state = STATES.JOINED
+        print("EVENT - Joined")
+
+
+def handler_timeout_tx(signal, frame):
+    global state
+    state = STATES.SEND_DATA
+
+
+def handler_sigint(signal, frame):
+    sys.exit(0)
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -12,8 +41,15 @@ if __name__ == "__main__":
         )
     port = str(sys.argv[1])
 
+    # Prepare signal management
+    signal.signal(signal.SIGALRM, handler_timeout_tx)
+    signal.signal(signal.SIGINT, handler_sigint)
+
     device = RAK3172(
-        serial_port=port, network_mode=RAK3172.NETWORK_MODES.LORAWAN, verbose=True
+        serial_port=port,
+        network_mode=RAK3172.NETWORK_MODES.LORAWAN,
+        verbose=True,
+        callback_events=events,
     )
     # device.joineui = "0102030405060708"
     # device.appkey = "11111111111111111111111111111113"
@@ -25,9 +61,18 @@ if __name__ == "__main__":
 
     # Join the network
     device.join()
+    state = STATES.JOINING
     while device.join_status() is not RAK3172.JOIN_STATUS.JOINED:
         False
-    print("Device has joined the network")
 
-    # Send a payload
-    device.send_payload(2, b"FEED")
+    while True:
+        if state == STATES.JOINED:
+            print("Device has joined the network")
+            state = STATES.SEND_DATA
+        elif state == STATES.SEND_DATA:
+            # Send a payload
+            device.send_payload(2, b"FEED")
+            signal.alarm(10)
+            state = STATES.SLEEP
+        elif state == STATES.SLEEP:
+            pass
--- rak3172.py
+++ rak3172.py
@@ -22,9 +22,13 @@ class RAK3172:
         NOT_JOINED = 0
         JOINED = 1
 
-    def __init__(self, serial_port, network_mode, verbose=False):
+    class EVENTS:
+        JOINED = 0
+
+    def __init__(self, serial_port, network_mode, verbose=False, callback_events=None):
         self.serial_port = serial_port
         self.verbose = verbose
+        self.__callback_events = callback_events
 
         # Open serial port
         try:
@@ -48,7 +52,7 @@ class RAK3172:
     @network_mode.setter
     def network_mode(self, network_mode):
         ans = self.send_command("AT+NWM=?")
-        if ans[-2] != "OK":
+        if ans[-1] != "OK":
             print("ERROR - Unable to check network mode")
             exit()
         if int(ans[0]) != network_mode:
@@ -58,7 +62,7 @@ class RAK3172:
     @property
     def appkey(self):
         ans = self.send_command("AT+APPKEY=?")
-        if ans[-2] != "OK":
+        if ans[-1] != "OK":
             print("ERROR - Unable to get APPKEY")
             exit()
         return ans[0].upper()
@@ -66,7 +70,7 @@ class RAK3172:
     @appkey.setter
     def appkey(self, appkey):
         ans = self.send_command(f"AT+APPKEY={appkey}")
-        if ans[-2] != "OK":
+        if ans[-1] != "OK":
             print("ERROR - Unable to set AppKEY")
             exit()
         # RAK3172 needs to be restarted to take it into account
@@ -75,7 +79,7 @@ class RAK3172:
     @property
     def deveui(self):
         ans = self.send_command("AT+DEVEUI=?")
-        if ans[-2] != "OK":
+        if ans[-1] != "OK":
             print("ERROR - Unable to get devEUI")
             exit()
         return ans[0].upper()
@@ -85,7 +89,7 @@ class RAK3172:
     @property
     def joineui(self):
         ans = self.send_command("AT+APPEUI=?")
-        if ans[-2] != "OK":
+        if ans[-1] != "OK":
             print("ERROR - Unable to get joinEUI")
             exit()
         return ans[0].upper()
@@ -93,7 +97,7 @@ class RAK3172:
     @joineui.setter
     def joineui(self, joineui):
         ans = self.send_command(f"AT+APPEUI={joineui}")
-        if ans[-2] != "OK":
+        if ans[-1] != "OK":
             print("ERROR - Unable to set joinEUI")
             exit()
         # RAK3172 needs to be restarted to take it into account
@@ -117,15 +121,15 @@ class RAK3172:
 
     def join(self):
         ans = self.send_command(f"AT+NJM={RAK3172.JOIN_MODES.OTAA}")
-        if ans[-2] != "OK":
+        if ans[-1] != "OK":
             print("ERROR - Unable to get join mode")
         ans = self.send_command("AT+JOIN=1:0:8:0")
-        if ans[-2] != "OK":
+        if ans[-1] != "OK":
             print("ERROR - Unable to join")
 
     def join_status(self):
         ans = self.send_command(f"AT+NJS=?")
-        if ans[-2] != "OK":
+        if ans[-1] != "OK":
             print("ERROR - Unable to get join status")
         return int(ans[0])
 
@@ -142,25 +146,37 @@ class RAK3172:
             print(f"-> {cmd.upper()}")
 
         self.serial.flush()
-        b = b""
+        # TODO - Asynchronous RX
         if ignore == False:
             received = self.serial.read(300)
             received = received.decode("ASCII").split("\r\n")
+            data = []
+            events = []
+            for element in received:
+                if not len(element):
+                    continue
+                if element[0] == "+":
+                    events.append(element)
+                else:
+                    data.append(element)
+            for event in events:
+                if event == "+EVT:JOINED":
+                    self.__callback_events(RAK3172.EVENTS.JOINED, None)
             if self.verbose is True:
-                print(f"<- {[x.upper() for x in received]}")
-            return received
+                print(f"<- {[x.upper() for x in data]}")
+            return data
         else:
             return None
 
     def send_payload(self, fport, payload, confirmed=False):
         # TODO - Implement confirm messages
         ans = self.send_command(f'AT+SEND={fport}:{payload.decode("ASCII")}')
-        if ans[-2] != "OK":
+        if ans[-1] != "OK":
             print("ERROR - Unable to send payload")
 
     def status(self):
         ans = self.send_command("AT")
-        if ans[-2] == "OK":
+        if ans[-1] == "OK":
             return True
         else:
             return False
